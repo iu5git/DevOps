d4073f6cba24da27fd60dfd6da2fd3b8acd22987
Anton <balashov@lab80>
2022-08-31 23:01:04 +0300

    Лаб3- начало

---
+++ Лаб3/python-ui-database/app/app.py
@@ -0,0 +1,34 @@
+import os
+from typing import List, Dict
+from flask import Flask
+import mysql.connector
+import json
+
+app = Flask(__name__)
+
+
+def favorite_colors() -> List[Dict]:
+    config = {
+        'user': os.environ.get('MYSQL_USER'),
+        'password': os.environ.get('MYSQL_ROOT_PASSWORD'),
+        'host': os.environ.get('MYSQL_HOST'),
+        'port': os.environ.get('MYSQL_PORT'),
+        'database': 'knights'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_colors')
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+@app.route('/')
+def index() -> str:
+    return json.dumps({'favorite_colors': favorite_colors()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
