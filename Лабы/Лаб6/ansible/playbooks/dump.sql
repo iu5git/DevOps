d2ed1b5850b4bd2f8e1ab227102d933b1daf92e1
balashov <balashov@lab80.com>
2022-11-05 23:34:32 +0300

    ansible

---
+++ Лабы/Лаб6/ansible/playbooks/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (
+  message varchar(255) NOT NULL
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  INSERT INTO test(message) VALUES('Ansible To Do List');
+  INSERT INTO test(message) VALUES('Get ready');
+  INSERT INTO test(message) VALUES('Ansible is fun')
